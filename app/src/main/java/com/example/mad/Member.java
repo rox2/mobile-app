12f7b062971a9c29d4b20c246462bc9c9e76cce9
Rashmika Mudalinayake <49615411+rox2@users.noreply.github.com>
2019-09-22 17:35:36 +0530

    step_count.err

---
+++ app/src/main/java/com/example/mad/Member.java
@@ -0,0 +1,18 @@
+package com.example.mad;
+
+public class Member {
+
+    private float ht;
+
+    public Member(){
+
+    }
+
+    public float getHt() {
+        return ht;
+    }
+
+    public void setHt(float ht) {
+        this.ht = ht;
+    }
+}
